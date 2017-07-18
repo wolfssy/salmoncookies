58938775dfa8c3d3e2c7adc68c73c8247c02c817
Brandon Arroyo <brandon.jarroyo@outlook.com>
2017-07-18 16:25:11 -0700

    grid good, can't align random cookies with times yet

--- app.js
+++ app.js
@@ -10,48 +10,48 @@
 var timeOfDay = ['0600','0700', '0800', '0900', '1000', '1100', '1200', '1300', '1400', '1500', '1600', '1700', '1800', '1900', '2000'];
 
 // /////////////////////////////////       PIKE      ///////////////////////////////////////
-//
-// var pike = {
-//   minCust: 23,
-//   maxCust: 65,
-//   avgCook: 6.3,
-//   hourC: [],
-//   randomPeeps: function (min,max){
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust
-//   },
-//   cookiesPerHour: function(){
-//     for(var i = 0; i < timeOfDay.length; i++){
-//       var cookiesPer = Math.floor(this.avgCook * this.randomPeeps());
-//       this.hourC.push(cookiesPer);
-//     }
-//     return this.hourC;
-//   },
-//   cookiesPD: function(){
-//     var total = 0
-//     for(var i = 0; i < this.hourC.length; i++){
-//       total += this.hourC[i];
-//     }
-//     return total;
-//   },
-//   render: function(){
-//     var pikeUl = document.getElementById('pike');
-//     for(var i = 0; i < timeOfDay.length; i++) {
-//       var liEl = document.createElement('li');
-//       liEl.textContent = timeOfDay[i] + ': ' + this.hourC[i] + ' Cookies per hour';
-//       pikeUl.appendChild(liEl);
-//     }
-//     var total = document.getElementById('pike');
-//     var liEl = document.createElement('li');
-//     liEl.textContent = 'total: ' + this.cookiesPD(total);
-//     total.appendChild(liEl);
-//   }
-//
-// };
-//
-// pike.randomPeeps();
-// pike.cookiesPerHour();
-// pike.render();
-//
+
+var pike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCook: 6.3,
+  hourC: [],
+  randomPeeps: function (min,max){
+    return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust
+  },
+  cookiesPerHour: function(){
+    for(var i = 0; i < timeOfDay.length; i++){
+      var cookiesPer = Math.floor(this.avgCook * this.randomPeeps());
+      this.hourC.push(cookiesPer);
+    }
+    return this.hourC;
+  },
+  cookiesPD: function(){
+    var total = 0
+    for(var i = 0; i < this.hourC.length; i++){
+      total += this.hourC[i];
+    }
+    return total;
+  },
+  render: function(){
+    var pikeUl = document.getElementById('pike');
+    for(var i = 0; i < timeOfDay.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = timeOfDay[i] + ': ' + this.hourC[i] + ' Cookies per hour';
+      pikeUl.appendChild(liEl);
+    }
+    var total = document.getElementById('pike');
+    var liEl = document.createElement('li');
+    liEl.textContent = 'total: ' + this.cookiesPD(total);
+    total.appendChild(liEl);
+  }
+
+};
+
+pike.randomPeeps();
+pike.cookiesPerHour();
+pike.render();
+
 // //////////////////////////////////   SeaTac Airport     ////////////////////////
 //
 // var Seatac = {
@@ -258,7 +258,7 @@ function Salmon(name,minCust, maxCust, avgCook){
     salmonTable.appendChild(trEl);
   }
 }
-  new Salmon('1st and Pike', 23, 65, 6.3);
+  new Salmon('1st and Pike', );
   new Salmon('SeaTac Airport', 3, 24, 1.2);
   new Salmon('Seattle Center', 11, 38, 3.7);
   new Salmon('Capitol Hill', 20, 38, 2.3);
@@ -269,19 +269,63 @@ function makeHeaderRow(){
   var trEl = document.createElement('tr');
 
   var thEl = document.createElement('th');
-  thEl.textContent = this.name;
+  thEl.textContent = 'Name';
   trEl.appendChild(thEl);
 
   thEl = document.createElement('th');
-  thEl.textContent = this.minCust;
+  thEl.textContent = '0600';
   trEl.appendChild(thEl);
 
   thEl = document.createElement('th');
-  thEl.textContent = this.maxCust;
+  thEl.textContent = '0700';
   trEl.appendChild(thEl);
 
   thEl = document.createElement('th');
-  thEl.textContent = this.avgCook;
+  thEl.textContent = '0800';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '0900';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1000';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1100';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1200';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1300';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1400';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1500';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1600';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1700';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1800';
+  trEl.appendChild(thEl);
+
+  thEl = document.createElement('th');
+  thEl.textContent = '1900';
   trEl.appendChild(thEl);
 
   salmonTable.appendChild(trEl);
@@ -293,6 +337,9 @@ function salmonRows(){
     allSalmon[i].render();
   }
 }
+
+makeHeaderRow();
+salmonRows();
 // What could that array for? Why would I need one?  to collect  our data
 
 
